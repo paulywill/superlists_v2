33ed08c31ce759a950cf14dbdc540f65f2f11fc9
Paul Gamble <pgamble@gmail.com>
2018-10-07 15:33:06 -0400

    Pylint in VScode

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+'''
+Functional Test for User Stories
+'''
 import time
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
-
+    ''' New Vistor Test Story'''
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -12,20 +15,21 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        '''Test for data retrieval'''
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do',self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do itme straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -36,10 +40,12 @@ class NewVisitorTest(unittest.TestCase):
         # "!: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),f"New to-do item did not appear in table. Contents were:\n{table.text}"
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
         )
 
         # There is still a text box inviting her to add another item. She
