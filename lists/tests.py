868711411b7ec009dd9d467678611019263458dd
Paul Gamble <pgamble@gmail.com>
2018-10-15 22:21:11 -0400

    Model for list Items and assoc migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from lists.models import Item
 
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
@@ -10,9 +11,11 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')    
+        self.assertTemplateUsed(response, 'home.html')
 
 class ItemModelTest(TestCase):
+        
+    def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.save()
@@ -27,4 +30,4 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')    
+        self.assertEqual(second_saved_item.text, 'Item the second')
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from lists.models import Item
 
 def home_page(request):
     return render(request, 'home.html', {
