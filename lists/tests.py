d0e89b8bd95ec65c0f7a7a9095312f05e76816d2
Paul Gamble <pgamble@gmail.com>
2018-11-18 00:22:00 -0500

    Chp7....some refactor...bookmarked on kindle

--- lists/tests.py
+++ lists/tests.py
@@ -17,7 +17,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})    
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -40,9 +40,18 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+    
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemy 1')
-        Item.objects.create(text='itemy 2')
-        response = self.client.get('/')
-        self.assertIn('itemy 1',response.content.decode())
-        self.assertIn('itemy 2', response.content.decode())
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
