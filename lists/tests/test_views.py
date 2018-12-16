63ca0f80b8a52b3ece151b78408b409a8bfe031b
Paul Gamble <pgamble@gmail.com>
2018-12-16 00:36:48 -0500

    synced with book, chapter 13

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils.html import escape
 from lists.models import Item, List
 
 
@@ -24,6 +25,12 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 class ListViewTest(TestCase):
 
@@ -81,3 +88,5 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+    
+    
