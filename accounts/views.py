6f9e7cf8b323d055cd78ccaaf0e2d1af95383ecb
Paul Gamble <pgamble@gmail.com>
2019-02-20 00:26:13 -0500

    (ch17|046)

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-
+from django.contrib import auth, messages
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from django.contrib import messages
+
 from accounts.models import Token
 
 def send_login_email(request):
@@ -25,10 +25,8 @@ def send_login_email(request):
     return redirect('/')
 
 def login(request):
-    print('login view', file=sys.stderr)
-    uid = request.GET.get('uid')
-    user = auth.authenticate(uid=uid)
-    if user is not None:
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
         auth.login(request, user)
     return redirect('/')
 
